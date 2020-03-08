304bf29046544f087045504a190fd9e20d0e9e5e
Holly "Frinkeldoodle" Lotor <admin@frinkel.tech>
2020-03-08 17:34:07 -0400

    Add OS detection, fix failing to detect users to be made, make it create users.

--- usermgmnt/batchacct.py
+++ usermgmnt/batchacct.py
@@ -8,9 +8,12 @@ import string
 import sys
 from os import path
 import os
+import platform
 
 # We'll need to be able to write to /dev/null, so open a writable stream to /dev/null.
 nullfeed = open(os.devnull, 'w')
+in_ubuntu = ("ubuntu" in platform.platform().lower())
+in_centos = ("centos" in platform.platform().lower())
 
 
 def main():
@@ -65,9 +68,12 @@ def main():
                 # If the username exists, id returns without error, so create new username with random nums at end.
                 if does_exist == 0:
                     username = orig_username + str(random.randint(0, 99999))
+                elif any(username in s[0] for s in users):
+                    username = orig_username + str(random.randint(0, 99999))
+                    does_exist = 1
 
-            # Password will be 16 random characters, with each character being a letter, a number, or punctuation.
-            password = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=16))
+            # Password will be 24 random characters, with each character being a letter or a number.
+            password = ''.join(random.choices(string.ascii_letters + string.digits, k=24))
 
             # Put the username, password, and arbitrarily-selected group into a list for further processing.
             users.append([username, password, usergroup])
@@ -84,7 +90,7 @@ def main():
     # Temps = Limit disk usage
 
     # Bail if doing a dry run.
-    if sys.argv[2] in ("-d", "--dry-run"):
+    if (len(sys.argv) > 2) and (sys.argv[2] in ("-d", "--dry-run")):
         print("Accounts Created (DRY RUN)")
         print("=======================================")
         for newUser in users:
@@ -105,6 +111,63 @@ def main():
         # Aw yeah, it's big "plz don't screw this up" time.
         print("Accounts Created")
         print("=======================================")
+        for newUser in users:
+            if newUser[2] == 0:
+                user_role = "administrators"
+                user_skeleton = "/etc/skel-0"
+                user_shell = "/bin/bash"
+            elif newUser[2] < 3:
+                user_role = "developer"
+                user_skeleton = "/etc/skel-2"
+                user_shell = "/bin/csh"
+            elif newUser[2] < 5:
+                user_role = "staff"
+                user_skeleton = "/etc/skel-4"
+                user_shell = "/bin/bash"
+            elif newUser[2] == 5:
+                user_role = "temp"
+                user_skeleton = "/etc/skel-5"
+                user_shell = "/bin/bash"
+            else:
+                # We should literally never get here, but if I don't add it, we would.
+                # Hence why I'm adding it lol
+                user_role = "Invalid"
+                print("An internal error has occurred and user {} could not be added.".format(newUser[0]))
+            if user_role != "Invalid":
+                user_creation = subprocess.run("/usr/sbin/useradd -m -k {skelly} -G {group} -s {usrshell} {username}"
+                                               .format(username=newUser[0], skelly=user_skeleton, group=user_role,
+                                                       usrshell=user_shell),
+                                               shell=True, stdout=nullfeed, stderr=nullfeed)
+                if user_creation.returncode != 0:
+                    print("An error occurred trying to add user {}".format(newUser[0]))
+                else:
+                    password_add = subprocess.run("echo '{username}:{userpass}' | /usr/sbin/chpasswd".format(
+                                                  username=newUser[0], userpass=newUser[1]),
+                                                  shell=True, stdout=nullfeed, stderr=nullfeed)
+                    if password_add.returncode != 0:
+                        print("An error occurred trying to assign password to user {}. "
+                              "This must be done manually.".format(newUser[0]))
+                    if user_role == "temp":
+                        # Add quota.
+                        if in_ubuntu:
+                            # Work with quotatool here
+                            quota_create = subprocess.run("/usr/sbin/quotatool -u {usern} -b -q 25Mb -l 30Mb /".format(
+                                                          usern=newUser[0]), shell=True, stdout=nullfeed,
+                                                          stderr=nullfeed)
+                            failed_quota = False
+                        elif in_centos:
+                            # Work with xfs quota here
+                            quota_create = subprocess.run("/usr/sbin/xfs_quota -x -c "
+                                                          "'limit -u bsoft=25m bhard=30m {}' /home".format(newUser[0]),
+                                                          shell=True, stdout=nullfeed, stderr=nullfeed)
+                            failed_quota = False
+                        else:
+                            print("Can't determine which OS you're on!")
+                            failed_quota = True
+                        if failed_quota or (quota_create.returncode != 0):
+                            print("Failed to create quota for user {}".format(newUser[0]))
+            print("{user} | {userpass} | {group}".format(user=newUser[0], userpass=newUser[1], group=user_role))
+
 
 # Ensure this  is being run as a script, not imported as a module.
 if __name__ == "__main__":
